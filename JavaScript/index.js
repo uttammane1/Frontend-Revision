77d19ac688e13f80ac861766d9ea1d5d580133e7
uttammane1 <uttammaneofficial@gmail.com>
2025-01-19 19:56:24 +0530

    opretors

---
+++ JavaScript/index.js
@@ -0,0 +1,102 @@
+// var a = 1;
+// var b = 2;
+
+// // console.log(a+b);
+
+// var c = "Uttam";
+
+// // console.log(c);
+
+// let name = "Uttam";
+//     name = "Abhijeet";
+    // console.log(name);   // Reassinging the value 
+
+
+
+// var d = "Uttam"
+// var e = 25;
+
+// // console.log(d, e);
+
+// {
+//     var a = 10;  // Global Scoped 
+// }
+
+// console.log(a);
+
+
+//  {
+//     let a = 10;  // block scoped
+    //    const b = 50;
+//  }
+//    console.log(b);
+// console.log(a);
+
+
+// let a = 20;
+// let b = 10;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+//  let firstName = "Uttam";
+//  let lastName = "Mane";
+
+//  console.log(firstName + " " + "-" + " " +  lastName)
+
+// let a = "5";
+// let b = "2";
+
+// console.log(a+b);
+// console.log(a-b);
+
+//cart 
+var product1 = 200;
+var quantity1 = 2;
+
+var product2 = 400;
+var quantity2 = 3;
+
+// total = 1600
+
+var productTotal1 = product1 * quantity1 
+// console.log(productTotal1)
+
+var productTotal2 = product2 * quantity2
+// console.log(productTotal2)
+
+// console.log(productTotal1 + productTotal2)
+
+
+// let a = 2;
+// let b = 5;
+// let c = 6;
+// let d = 8;
+
+// let eq = (a+b)*(c-d);
+
+// console.log(eq);
+
+// let a = 3;
+
+// console.log(a**6/2);
+
+
+// let mod = 10 % 3;
+
+// console.log(mod)
+
+// let num1 = 100;
+// let num2 = 99;
+
+// console.log(num1 % 2);
+// console.log(num2 % 2);
+
+// var a = 20;
+// var a = 14;
+// var a = "name";
+
+// console.log(a);
+
