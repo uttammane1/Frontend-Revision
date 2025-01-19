f3450bc1d71b912ab6ee6fdf7461c2fdca7a8d6b
uttammane1 <uttammaneofficial@gmail.com>
2025-01-19 20:42:57 +0530

    Opretors Done

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -100,3 +100,231 @@ var productTotal2 = product2 * quantity2
 
 // console.log(a);
 
+// let a = 2;
+// let b = 4;
+
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a == b);
+
+// let a = 20;
+
+// console.log(a <=19)
+
+// let a = 5;
+// let b = 5;
+// console.log(a >= b);
+// console.log(b > a);
+
+// let a = 2;
+// console.log(a**3);
+
+let a = 3;
+
+console.log(3*3+10)
+
+
+
+
+
+
+
+// lecture code 
+
+
+
+
+
+
+
+
+// // operators => mathematical and relational
+
+// // variables => var (let , const  )
+
+// // datatype => number , string
+
+// // diff b/w loosely typed and strictly typed lang .
+
+// // loosely typed we don't need to tell the datatype
+
+
+// var a = 10;
+
+// a = "10"
+
+
+// // Int name = 12;
+// // name = "ritesh"
+// var b = 10;
+
+// // 10 or "10"
+
+// console.log(typeof a , typeof b )
+
+// // typeof  => inbuilt
+
+
+// var num = 10;
+
+// num = "ten"
+
+// String name = "ritesh"
+
+
+// Scoping => global and block
+
+
+// {
+
+//   var a = 10 ; 
+  
+  
+// }
+
+// console.log(a)
+
+// {
+//   const a = 10;
+// }
+
+// console.log(a)
+
+// var is global
+
+// let and const are block
+
+// {
+// // let b = 10 ; 
+// //  b = 20;
+// //   console.log(b)
+//   let name = "ritesh"
+//   name = "vikas"
+//   console.log(name)
+
+// we can assign (or reassign) values to let whereas we can't do the same in const
+
+//   let adhar = "1234567890"
+//   adhar = "98764362"
+
+//   const pan = "jsk123"
+//   pan = "skj321"
+//   console.log(pan)
+  
+// }
+
+// var $hello = "hello world"
+
+// $ and _ we can use these
+
+// var countryName = "india1"  // camel case
+// var country_name = "india2" // snake case
+// var CountryName = "india3" // pascal case
+// // var country-name 
+
+// console.log(country_name)
+
+// Mathematical operator
+
+// // + , - , * , /
+
+// var x = 10 ; 
+// var y = 12 ;
+
+// var a = "3" ; // number
+// var b = "3" ; // string
+
+//  console.log(a+b) // type coercion
+
+// //concatination
+
+// // console.log(x+y)  //=> 22
+// // console.log(y-x) //=> 2
+// // console.log(x*y) //=> 120
+// // console.log(x/y) // => 0.8
+
+// var firstName = "ritesh"
+// var lastName = "ranjan"
+
+// console.log(firstName + lastName)
+// NaN => not a no.
+
+// + => concatination
+
+// //cart 
+// var product1 = 200;
+// var quantity1 = 2;
+
+// var product2 = 400;
+// var quantity2 = 3;
+
+// // total = 1600
+
+// var productTotal1 = product1 * quantity1 
+// console.log(productTotal1)
+
+// var productTotal2 = product2 * quantity2
+// console.log(productTotal2)
+
+// console.log(productTotal1 + productTotal2)
+
+// // let a = 2;
+// // let b = 3;
+// // let c = 4;
+// // let d = 5;
+
+
+// // let eq = a-b*c/d
+// // console.log(eq)
+// // var a = 3;
+
+// // console.log(9**(1/2))
+
+// // *   and   ** 
+
+// // modulo-> %
+// // var mod = 5 % 2
+
+// // console.log(mod)
+
+// // -4/5
+
+// var num1 = 22 // even
+// var num2 = 23 // odd
+
+// console.log(num2%2)
+
+// var a = 19;
+// a = 20
+// a = "str"
+
+// // String name = value;
+// // name = 10;
+
+// console.log(a)
+
+// var PascalValue = "store"
+// var pascalValue = "store"
+
+// var pascal-value = "store"
+
+//Relational or comparison operator
+
+// var a = 4
+// var b = 4
+
+// var num = 16;
+
+// // if num is less than or equal to 15
+
+// var num2 = 6;
+// // if num2 is greater than or equal to 7;
+
+// console.log(num<=15)
+
+// console.log(num2>=7)
+
+// a>b
+//a = b
+
+// console.log(a>b)
