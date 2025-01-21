b5f9c332450ebcd125fa561e00c7181b46fa2f71
uttammane1 <uttammaneofficial@gmail.com>
2025-01-21 18:24:54 +0530

    logical operator

---
+++ JavaScript/ternary.js
@@ -0,0 +1,221 @@
+// Ternary Operator
+
+// let a = 60;
+// let b = 50;
+
+// a == b ? console.log("Right") : a <= b ? console.log("minimum") : console.log("wrong");
+
+// Logical Oprator  => And && , Or || , Not !
+
+// let a = 20;
+// let b = 20;
+// let c = 60;
+// let d = 80;
+
+// console.log((a == b) && (a > b))
+// console.log((c == d) || (c < d))
+
+// console.log(true && true)
+// console.log(true && false)
+
+// console.log(true || false)
+
+
+
+
+// let user = "Uttam";
+// let pass = "123";
+// let que = "Village"
+
+// let dbUser = "Mane";
+// let dbPass = "1234";
+// let dbQue = "Village";
+
+// if ((user == dbUser) && (pass == dbPass)){
+//     console.log("Welcome Back")
+// }else{
+//     console.log("Wrong UserName or Password")
+// }
+
+// if ((user == dbUser) && (pass == dbPass) || que == dbQue){
+//     console.log("Welcome Back")
+// }else{
+//     console.log("Wrong UserName or Password")
+// }
+
+
+
+// if you get a overall 80%+ and in maths 70+ you get marks then you get bike, not if you cant get the marks dont get a bike
+
+// let overall = 85;
+// let maths = 80;
+
+// if ((overall >= 80) && (maths >= 70)){
+//     console.log("Get a Bike")
+// }else{
+//     console.log("not anything")
+// }
+
+
+// Marriage example
+
+// let male = 21;
+// let female = 17;
+
+// if ((male >= 21) && (female >= 18)){
+//     console.log("You can Register")
+// }else{
+//     console.log("You Can't marrying")
+// }
+
+
+// day was const 1 - 7 its a constant thats why using switch case 
+
+// let day = 10;
+
+// switch (day){
+//     case 1 : console.log("One")
+//             break;
+    
+//     case 2 : console.log("Two")
+//             break;        
+
+//     case 3 : console.log("Three")
+//             break;        
+            
+//     case 4 : console.log("Four")
+//             break;        
+
+//     case 5 : console.log("Five")
+//             break;        
+
+//     case 6 : console.log("Six")
+//             break;        
+
+//     case 7 : console.log("Seven")
+//             break;     
+            
+//     default : console.log("Invalid")        
+// }
+
+
+
+
+// let day = 6;
+
+// if (day === 1) {
+//   console.log("One");
+// } else if (day === 2) {
+//   console.log("Two");
+// } else if (day === 3) {
+//   console.log("Three");
+// } else if (day === 4) {
+//   console.log("Four");
+// } else if (day === 5) {
+//   console.log("Five");
+// } else if (day === 6) {
+//   console.log("Six");
+// } else if (day === 7) {
+//   console.log("Seven");
+// } else {
+//   console.log("Invalid");
+// }
+
+
+// days matching
+
+
+// let day = 8;
+
+// switch (day){
+//      case 1 : console.log("Sun")
+//              break;
+
+//      case 2 : console.log("Mon")
+//              break;
+
+//      case 3 : console.log("Tue")
+//              break;
+
+//      case 4 : console.log("Wed")
+//              break;
+
+//      case 5 : console.log("Thu")
+//              break;
+
+//      case 6 : console.log("Fri")
+//              break;
+
+//      case 7 : console.log("Sat")
+//              break;
+
+//      default : console.log("Wrong")        
+// }             
+
+
+// 13 to 19 is a teenage and 20 to 29 is thwentys
+
+// let age = 25;
+// if (age >=13 && age <= 19 ){
+//     console.log("Teenage")
+// }else if(age >= 20 && age <= 29){
+//     console.log("Twentys")
+// }
+
+
+
+// Vowel = a, e, i, o, u      => Not Vowel    =>  consonent 
+
+// var char = "O";
+// char = char.toLowerCase()
+
+// if ( char == "a" || char == "e" || char == "i" || char == "o" || char == "u"){
+//     console.log("Its Vowel")
+// }else{
+//     console.log("Its Consonent")
+// }
+
+
+// take 3 numbers and print the big number 
+
+// let a = 5;
+// let b = 7;
+// let c = 1;
+
+// if ((a > b) && (a > c)){
+//     console.log("A is greater");
+// }else if ((b > a) && (b > c)){
+//     console.log("B is greater")
+// }else if ((c > a) && (c > b)){
+//     console.log("C is greater")
+// }
+
+
+// let a = 5;
+// let b = 7;
+// let c = 9;
+
+// ((a > b) && (a > c)) ? console.log("A is greater") : ((b > a) && (b > c)) ? console.log("B is greater") : console.log("C is greater")
+
+
+
+// let value = 15;
+// let a = 1;
+
+// while(a <= value){
+//     console.log("Uttam Mane")
+//     a = a + 1;
+//     console.log(a)
+// }
+
+
+// Factorial 5*4*3*2*1
+
+// var num = 20;
+// var prod = 1;
+
+// while(num > 0){
+//     prod = prod * num
+//     num = num - 1
+//     console.log(prod)
+// }
