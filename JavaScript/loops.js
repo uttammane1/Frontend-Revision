dbd9bf9e7aa2c65a0ffaf829006a8392464ab7b5
uttammane1 <uttammaneofficial@gmail.com>
2025-01-25 19:22:19 +0530

    Done Loop

---
+++ JavaScript/loops.js
@@ -0,0 +1,162 @@
+// print 1 - 10
+
+// let a = 1;
+
+// while(a <= 10){
+//     console.log(a)
+//     a = a +1
+//     // console.log(a)
+// }
+
+
+// let a = 1;
+
+// while(a <= 10){
+//     console.log(a)
+//     a = a +1
+
+// }
+
+// let a = 5;
+
+// a ++
+
+// console.log(a);
+
+// let num = 1;
+// while(num < 11){
+//     console.log(num ++)  // This one is printing 1 - 10 because of afte the adding 1
+// }
+
+// let num2 = 1;
+// while(num2 < 11){
+//     console.log(++num2)     // This One Is Printing 2 - 11 because of before the printing adding 1
+// }
+
+// let a = 2;
+// console.log(--a)     befor printing the getting minus thats why printing 1
+
+// let a = 5;
+// console.log(a--)     after the printing that is getting a minus thats why output is same like input
+
+
+// let num = 1;
+// while(num < 10){
+//     num *= 2
+//     console.log(num)
+// }
+
+// let num = 1;
+// while(num < 10){
+//     num /= 2
+//     console.log(num)
+// }
+
+
+// print 1 => 20 Even Numbers
+
+// var num = 1
+// while(num <= 20){
+//     if(num%2 == 0){
+//     console.log(num)
+// }
+// num ++
+// }
+
+
+// var num = 1
+// while(num <= 20){
+//     if (num%2 == 0){
+//         console.log(num)
+//     }
+//     num++
+// }
+
+
+//  Printing Even or Odd
+
+
+//  var num = 1
+// while(num <= 50){
+//     if (num%5 == 0){
+//         console.log(num)
+//     // }else{
+//     //     console.log("Odd")
+//      }
+//     num++
+// }
+
+
+// var num = 1;
+
+// while(num <= 20){
+//     num*= 2
+//     console.log(num);
+        
+// }
+
+
+// let num =5;
+// while(num >=0){
+//     console.log(num)
+//     num--
+// }
+
+// for (var i = 0; i<=10; i++){
+//     console.log(i)
+// }
+
+// 2 => 22
+
+// for (var a = 2; a <= 22; a*=2){
+//     console.log(a)
+// }
+
+// a = a*2
+
+
+// 5 >= 0
+
+// for (a = 5; a>=0; a--){
+//     console.log(a)
+// }
+
+// var str = " ";
+
+// for (var a = 0; a<=10; a++){
+//     str = str+ " " + a
+//     console.log(str)
+// }
+// console.log(str)
+
+// var prod = 1;
+// for(var a = 1; a<10; a++){
+//     prod *= a
+// }
+// console.log(prod)
+
+// for (var a = 1; a<10; a++){
+//     if (a == 5){
+//         continue
+// }
+// console.log(a)
+// }
+
+// for (i = 0; i< 5; i++){
+//     console.log("Masai School")
+// }
+
+// for (i = 1; i<= 5; i++){
+//     console.log(i)
+// }
+
+// for (a = 14; a<=20; a++){
+//     console.log(a)
+// }
+
+
+// for (var a = 1; a<=10; a++){
+    
+//     console.log(a*2)
+// }
+
